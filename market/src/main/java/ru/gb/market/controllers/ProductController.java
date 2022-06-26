f6f9e3a9750d5286f9d1eabd4a5c38e15aa003da
LowBob93 <Tester269@gmail.com>
2022-06-26 09:29:40 +0300

    HW7

---
+++ market/src/main/java/ru/gb/market/controllers/ProductController.java
@@ -0,0 +1,53 @@
+package ru.gb.market.controllers;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import ru.gb.market.DTO.ProductDto;
+import ru.gb.market.models.Product;
+import ru.gb.market.services.ProductService;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class ProductController {
+    private final ProductService productService;
+
+
+    @GetMapping("/products")
+    public List<ProductDto> findAll(@RequestParam(required = false)Integer min,
+                                    @RequestParam(required = false)Integer max) {
+        List<Product> products =productService.findWithParams(min,max);
+        List<ProductDto> productDto = new ArrayList<>();
+        for (Product p: products) {
+            productDto.add(new ProductDto(p));
+        }
+        return productDto;
+    }
+
+
+    @GetMapping("/products/{id}")
+    public ProductDto findById(@PathVariable Long id) {
+        return new ProductDto(productService.findById(id).get());
+    }
+
+    @PostMapping("products")
+    public ProductDto save(@RequestBody Product product) {
+        return new ProductDto(productService.save(product));
+    }
+
+    @GetMapping("products/delete/{id}")
+    public void  deleteById(@PathVariable Long id) {
+        productService.deleteById(id);
+    }
+
+
+   /* @GetMapping("/products/price")
+    public List<Product> findByPrice(@RequestParam(name = "min") int minPrice,
+                                     @RequestParam(name = "max") int maxPrice) {
+        return productService.findPriceBetween(minPrice, maxPrice);
+    } */
+
+}
