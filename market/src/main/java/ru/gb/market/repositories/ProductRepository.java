4cf8d6c2aa3c10546cf5c590c089853e4656ce89
LowBob93 <Tester269@gmail.com>
2022-06-26 06:45:07 +0300

    HomeWork 7

---
+++ market/src/main/java/ru/gb/market/repositories/ProductRepository.java
@@ -0,0 +1,18 @@
+package ru.gb.market.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import ru.gb.market.models.Product;
+
+import java.util.List;
+
+public interface ProductRepository extends JpaRepository<Product,Long> {
+    // @Query("select p from Product p where p.price <= :maxPrice and p.price >= :minPrice")
+    // List<Product> findAllByPriceAfterAndTitleContains(int minPrice, String s);
+    List<Product> findByPriceBetween(int minPrice,int maxPrice);
+    List<Product> findByTittle(String tittle);
+    List<Product> findByPriceBefore(int max);
+    List<Product> findByPriceAfter(int min);
+
+
+}
---
+++ market/src/main/java/ru/gb/market/services/ProductService.java
@@ -0,0 +1,50 @@
+package ru.gb.market.services;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import ru.gb.market.models.Product;
+import ru.gb.market.repositories.ProductRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class ProductService {
+    public final ProductRepository productRepository;
+
+    public List<Product> findAll(){
+        return productRepository.findAll();
+    }
+
+    public Optional<Product> findById(Long id){
+        return productRepository.findById(id);
+
+    }
+    public Product save(Product product){
+        return productRepository.save(product);
+    }
+    public List<Product>findPriceBetween(int minPrice, int maxPrice){
+        return productRepository.findByPriceBetween(minPrice,maxPrice);
+    }
+
+    public List<Product> findByTitle(String title){
+        return productRepository.findByTittle(title);
+    }
+    public void deleteById(Long id) {
+        productRepository.deleteById(id);
+    }
+    public List<Product> findWithParams(Integer min,Integer max){
+        if (min == null && max ==null){
+            return productRepository.findAll();}
+        else if (min == null && max !=null){
+            return  productRepository.findByPriceBefore(max);}
+        else if (min != null && max==null ){
+            return productRepository.findByPriceAfter(min);}
+        else {
+            return  productRepository.findByPriceBetween(min,max);
+            }
+        }
+    }
+
+
